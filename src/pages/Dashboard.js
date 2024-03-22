dd10dd646b4f7294dc5ec173275bb160ed12b951
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2024-03-22 16:25:43 +0530

    new

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import Cards from '../components/Cards';
+import AddExpenseModal from '../components/Modals/addExpense';
+import AddIncomeModal from '../components/Modals/addIncome';
 
 const Dashboard = () => {
+  const [isExpenseModalVisible,setisExpenseModalVisible]=useState(false);
+  const [isIncomeModalVisible,setIsIncomeModalVisible]=useState(false);
+  const showExpenseModal=()=>{
+    setisExpenseModalVisible(true);
+  }
+  const showIncomeModal=()=>{
+    setIsIncomeModalVisible(true);
+  }
+  const handleExpenseCancel=()=>{
+    setisExpenseModalVisible(false);
+  }
+  const handleIncomeCancel=()=>{
+    setIsIncomeModalVisible(false);
+  };
+
+  const onFinish=(values,type)=>{
+    console.log("onFinish",values,type);
+  }
   return (
     <div>
       <Header/>
+      <Cards 
+      showExpenseModal={showExpenseModal}
+      showIncomeModal={showIncomeModal}
+      />
+      <AddExpenseModal
+      isExpenseModalVisible={isExpenseModalVisible}
+      handleExpenseCancel={handleExpenseCancel}
+      onFinish={onFinish}
+      />
+      <AddIncomeModal
+      isIncomeModalVisible={isIncomeModalVisible}
+      handleIncomeCancel={handleIncomeCancel}
+      onFinish={onFinish}
+      /> 
     </div>
   )
 }
